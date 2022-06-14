4b830394f81b2b1d59d75b5f1384fb0626fb933a
beck <beck314159@hotmail.com>
2022-06-13 19:29:17 -0700

    Actaully updates

--- script.js
+++ script.js
@@ -69,15 +69,27 @@ raw_overlay_2 = `2,2,2,2,2,2,2,2,5;0,4,4,4,4,2,2,2,2,2,2,2,2
 2,2,2,2,2,2,2,2,8;13,4,4,4,4,4,4,4,4,4,4,4,4
 2,2,2,2,2,2,2,2,8;14,4,4,4,4,4,4,4,4,4,4,4,4`
 
-// console.log(qr_code_array)
-
-// Zones:
-// 2 = red
-// 3 = orange
-// 4 = green
-// 5 = blue
-// 6 = purple
-
+raw_overlay_3 = `,,,,,,,,,1,,1,,,,,,,,,
+,,,,,,,,1,,1,,1,,,,,,,,
+,,,,,,,,,1,,1,,,,,,,,,
+,,,,,,,,1,,1,,1,,,,,,,,
+,,,,,,,,,1,,1,,,,,,,,,
+,,,,,,,,1,,1,,1,,,,,,,,
+,,,,,,,,,,,,,,,,,,,,
+,,,,,,,,,1,,1,,,,,,,,,
+,1,,1,,1,,,1,,1,,1,,1,,1,,1,,1
+1,,1,,1,,,1,,1,,1,,1,,1,,1,,1,
+,1,,1,,1,,,1,,1,,1,,1,,1,,1,,1
+1,,1,,1,,,1,,1,,1,,1,,1,,1,,1,
+,1,,1,,1,,,1,,1,,1,,1,,1,,1,,1
+,,,,,,,,,1,,1,,1,,1,,1,,1,
+,,,,,,,,,,1,,1,,1,,1,,1,,1
+,,,,,,,,,1,,1,,1,,1,,1,,1,
+,,,,,,,,,,1,,1,,1,,1,,1,,1
+,,,,,,,,,1,,1,,1,,1,,1,,1,
+,,,,,,,,,,1,,1,,1,,1,,1,,1
+,,,,,,,,,1,,1,,1,,1,,1,,1,
+,,,,,,,,,,1,,1,,1,,1,,1,,1`
 
 class QR_code_list {
   constructor(qr_code_list) {
@@ -94,7 +106,154 @@ class QR_code_list {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
     /// Use: await sleep(2);
-    // Function must be asinc
+    // Function must be async
+}
+
+class AnimatedQRcode {
+  constructor (canvas_link, default_qr_code_data_csv, overlay_qr_code_data_csv) {
+    this.canvas = canvas_link
+
+    this.qr_code_array_rows = default_qr_code_data_csv.split(/\r?\n/)
+    this.overlay_array_rows = overlay_qr_code_data_csv.split(/\r?\n/)
+
+    this.ctx = this.canvas.getContext("2d")
+    this.ctx.imageSmoothingEnabled = false
+
+    this.iscurrentlyanimating = false
+
+    for (let y=0; y < this.qr_code_array_rows.length; y++) {
+      let qr_code_array_columbs = this.qr_code_array_rows[y].split(",")
+      let color = ""
+
+      for (let x=0; x < qr_code_array_columbs.length; x++) {
+        if (qr_code_array_columbs[x] == "1") {
+          color="black"
+        } else {
+          color="white"
+        }
+        //console.log(this.grid_checkbox_id)
+        //console.log(document.querySelectorAll(this.grid_checkbox_id))
+        this.drawSquare(x,y,color)
+
+      }
+    }
+
+  }
+
+  async animate() {
+    console.log("Trying to animate!")
+
+    if (this.iscurrentlyanimating != true) {
+    this.iscurrentlyanimating = true
+    console.log("Started animation!")
+      for (let y=0; y < this.qr_code_array_rows.length; y++) {
+        let qr_code_array_columbs = this.qr_code_array_rows[y].split(",")
+        let color = ""
+
+        for (let x=0; x < qr_code_array_columbs.length; x++) {
+          if (qr_code_array_columbs[x] == "1") {
+            color="black"
+          } else {
+            color="white"
+          }
+          //console.log(this.grid_checkbox_id)
+          //console.log(document.querySelectorAll(this.grid_checkbox_id))
+          this.drawSquare(x,y,color)
+
+        }
+      }
+
+      for (let y=this.overlay_array_rows.length-1; y >= 0; y--) {
+        let overlay_array_columbs = this.overlay_array_rows[y].split(",")
+        let qr_code_array_columbs = this.qr_code_array_rows[y].split(",")
+
+        for (let x=overlay_array_columbs.length-1; x >= 0; x--) {
+          let data = overlay_array_columbs[x]
+          let waitTime = 1000 - document.querySelectorAll("#speedslider")[0].value
+          waitTime = waitTime + 50
+          let color = ""
+
+          let number_value = data
+          if (number_value == 1) {
+            await sleep(waitTime)
+
+            // Opposite colors so you can see the "F"'s
+            if (qr_code_array_columbs[x] == "1") {
+              color="white"
+            } else {
+              color="black"
+            }
+
+            // this.drawGrid((x-2),y, "green")
+            this.drawText(x,y,color, "F")
+            // if() {
+            //   this.drawGrid((x+2),y,"white")
+            // } else {
+            //   this.drawGrid((x+2),y,"black")
+            // }
+          }
+
+
+        }
+      }
+
+      for (let y=this.overlay_array_rows.length-1; y >= 0; y--) {
+        let overlay_array_columbs = this.overlay_array_rows[y].split(",")
+        let qr_code_array_columbs = this.qr_code_array_rows[y].split(",")
+
+        for (let x=overlay_array_columbs.length-1; x >= 0; x--) {
+          let data = overlay_array_columbs[x]
+          let waitTime = 1000 - document.querySelectorAll("#speedslider")[0].value
+          waitTime = waitTime + 50
+          let color = ""
+
+          let number_value = data
+          if (number_value == 1) {
+            await sleep(waitTime)
+
+            // Opposite colors so you can see the "F"'s
+            if (qr_code_array_columbs[x] == "1") {
+              color="white"
+            } else {
+              color="black"
+            }
+            this.drawSquare(x,y,color)
+          }
+
+
+        }
+      }
+    this.iscurrentlyanimating = false
+    console.log("Finished animation")
+    }
+
+  }
+
+  drawSquare(x, y, color) {
+    this.ctx.beginPath();
+    this.ctx.rect(x*20, y*20, 20, 20);
+    this.ctx.fillStyle = color;
+    this.ctx.fill();
+  }
+
+  drawText(x, y, color, text) {
+    this.ctx.beginPath();
+    this.ctx.font = "14px Comic Sans MS";
+    this.ctx.fillStyle = color;
+    this.ctx.textAlign = "center";
+    this.ctx.fillText(text, (x*20 - 10.7 + 20), (y*20 - 5 + 20));
+  }
+
+  drawGrid(x,y, color) {
+
+    this.ctx.beginPath();
+    this.ctx.lineWidth = "2";
+    this.ctx.strokeStyle = color;
+    this.ctx.rect(x*20, y*20 , 20, 20);
+    this.ctx.stroke();
+
+  }
+
 }
 
 class QRcode {
@@ -109,9 +268,6 @@ class QRcode {
 
     this.ctx = this.canvas.getContext("2d")
     this.ctx.imageSmoothingEnabled = false
-
-
-    this.update()
 
     // This is so cursed its not even funny...
     // Like I'm actaully evil for doing this.
@@ -209,6 +365,7 @@ class QRcode {
 
 canvas = document.getElementById("canvas");
 canvas2 = document.getElementById("canvas2");
+canvas3 = document.getElementById("canvas3");
 
 checkbox_id_list_1 = [
   [2, "#camera_alignment", "PaleVioletRed"],
@@ -228,7 +385,19 @@ checkbox_id_list_2 = [
   [8, "#formating_data2", "red"]
 ]
 
+checkbox_id_list_3 = [
+  [3, "#camera_alignment3", "orange"],
+]
+
 qr_code_1 = new QRcode(canvas, raw_data, raw_overlay_1, checkbox_id_list_1, "#grid")
 qr_code_2 = new QRcode(canvas2, raw_data, raw_overlay_2, checkbox_id_list_2, "#grid2")
 
 the_list = new QR_code_list([qr_code_1, qr_code_2])
+
+animatedQRcode_1 = new AnimatedQRcode(canvas3, raw_data, raw_overlay_3)
+
+button = document.getElementById("animationstartbutton");
+
+button.addEventListener('click', function(e) {
+  animatedQRcode_1.animate()
+})
